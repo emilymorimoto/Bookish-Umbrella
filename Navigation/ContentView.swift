01afc327adff0a4a08dfb59eebd6ade3b6dd02ad
Emily <morimoto.emily@Icloud.com>
2023-06-28 16:30:41 -0300

    Initial Commit

---
+++ Navigation/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Emily Morimoto on 27/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            NavigationLink(destination: Text("Second View")) {
+                Text("Hello, World!")
+            }
+            .navigationTitle("Navigation")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Emily Morimoto on 27/06/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
