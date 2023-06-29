dd7daf62e5e40b7b06d60ca56eacc2e0a85c7a80
Emily <morimoto.emily@Icloud.com>
2023-06-29 15:24:30 -0300

    atualizei o texto 3

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             NavigationLink(destination: Text("Second View")) {
-                Text("Hello, bonito-kun, pedro-cueca, bia-forknife!")
+                Text("Hello, bonito-kun, pedro-cueca, bia-forknifeee!")
             }
             .navigationTitle("Navigation")
         }
