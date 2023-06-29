7a67cb5d10ec6516e52fd5e09f9be394765be988
Emily <morimoto.emily@Icloud.com>
2023-06-29 13:36:27 -0300

    testandooo

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             NavigationLink(destination: Text("Second View")) {
-                Text("Hello, testando1!")
+                Text("Hello, testandoooo!")
             }
             .navigationTitle("Navigation")
         }
